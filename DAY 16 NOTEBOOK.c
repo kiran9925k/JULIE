b95a64ac5f9b0bee5bec08a223d543e88f372114
kiran9925k <kirankushwah1601@gmail.com>
2024-10-13 23:31:24 +0530

    Add files via upload

---
+++ DAY 16 NOTEBOOK.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a;
+	    scanf("%d",&a);
+	    printf("%d\n",a*10);
+	}
+	return 0;
+}
+
+
